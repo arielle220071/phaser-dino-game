fca1480ae9ba0b233da22ebd21c19099b3161bca
arielle220071 <220071@live.crescent.edu.sg>
2025-03-26 08:58:08 +0000

    until obstacles

--- game.js
+++ game.js
@@ -27,12 +27,24 @@ function preload() {
   this.load.spritesheet("dino", "assets/dino-idle.png", {frameWidth: 88, frameHeight: 94});
   this.load.image("ground","assets/ground.png");  
   this.load.image("clouds","assets/cloud.png"); 
+  for(let i =1; i<7; i++){
+    this.load.image(`obstacle${i}`, `assets/cactuses_${i}.png`)
+  }
 }
 
 function create() {
 
-    this.player = this.physics.add.sprite(200,200, "dino").setOrigin(0);
-    this.ground = this.add.tileSprite(0, 500, 1000, 30, "ground").setOrigin(0,1);dx
+    this.player = this.physics.add.sprite(200,200, "dino").setOrigin(0)
+    .setCollideWorldBounds(true)
+    .setBodySize(44,92)
+    .setGravityY(5000);
+    this.ground = this.add.tileSprite(0, 500, 1000, 30, "ground").setOrigin(0,1);
+    
+    this.groundCollider= this.physics.add.staticSprite(0, 500, "ground").setOrigin(0,1);
+    this.groundCollider.body.setSize(1000,30);
+
+    this.physics.add.collider(this.player, this.groundCollider);
+
     this.clouds = this.add.group();
     this.clouds = this.clouds.addMultiple(
         [this.add.image(300,130, "clouds"),
@@ -43,9 +55,14 @@ function create() {
             this.add.image(600,200, "clouds"),
             this.add.image(200,200, "clouds")]
         );
-    
+    this.gameSpeed =5;
+    this.obstacles = this.physics.add.group({
+        allowGravity:false
+    });
 }
 
 function update() {
-
+    this.ground.tilePositionX +=this.gameSpeed;
+    const num= Math.floor(Math.random() * 6) +1;
+    this.obstacles.create(750,390, `obstacle${num}`).setOrigin(0);
 }
